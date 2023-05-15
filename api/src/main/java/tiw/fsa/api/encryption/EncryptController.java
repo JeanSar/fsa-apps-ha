6fc2a732d764f6352fb722c91198dc2d1909767b
JeanSar <jeansar504@gmail.com>
2023-05-15 16:25:22 +0200

    adding gauge and counter

--- api/src/main/java/tiw/fsa/api/encryption/EncryptController.java
+++ api/src/main/java/tiw/fsa/api/encryption/EncryptController.java
@@ -1,9 +1,15 @@
 package tiw.fsa.api.encryption;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import tiw.fsa.api.security.ForbiddenAccessException;
 import tiw.fsa.api.security.SecurityUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Point d'accès pour la gestion des chiffrements
@@ -12,11 +18,22 @@ import tiw.fsa.api.security.SecurityUtils;
 @RequestMapping("/crypt")
 public class EncryptController {
     private final EncryptService encryptService;
+    @Autowired
+    private MeterRegistry meterRegistry;
+    private Gauge gaugeEncrypt;
+    private List<String> encryptList = new ArrayList<String>();
 
     public EncryptController(EncryptService encryptService) {
         this.encryptService = encryptService;
     }
 
+    @PostConstruct
+    public void init() {
+
+        gaugeEncrypt = Gauge.builder("encrypt_api_request", encryptList, List::size)
+                        .description("Gauge of encrypt request")
+                        .register(meterRegistry);
+    }
     /**
      * Encrypts data using the user's key. Data should be encoded using base64 encoding.
      *
@@ -29,6 +46,8 @@ public class EncryptController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String encrypt(@PathVariable("login") String login, @PathVariable("keyname") String keyname, @RequestBody String data) throws ForbiddenAccessException, NoSuchKeyException, WorkerException {
+        encryptList.add(login);
+        gaugeEncrypt.measure();
         SecurityUtils.checkCurrentUser(login);
         return encryptService.encrypt(login, keyname, data);
     }
--- worker/src/main/java/tiw/fsa/worker/EncryptionController.java
+++ worker/src/main/java/tiw/fsa/worker/EncryptionController.java
@@ -1,28 +1,65 @@
 package tiw.fsa.worker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/crypt/{keyName}")
 public class EncryptionController {
+    @Autowired
+    private MeterRegistry meterRegistry;
+    private Counter encryptInitCounter;
+    private Counter encryptEndCounter;
+    private Counter decryptInitCounter;
+    private Counter decryptEndCounter;
+
     private final EncryptionService encryptionService;
 
     public EncryptionController(EncryptionService encryptionService) {
         this.encryptionService = encryptionService;
     }
 
+    @PostConstruct
+    public void init() {
+        encryptInitCounter =
+                Counter.builder("encrypt_init_worker_request")
+                        .description("Number of encrypt request initiated on worker")
+                        .register(meterRegistry);
+        decryptInitCounter =
+                Counter.builder("decrypt_init_worker_request")
+                        .description("Number of decrypt request initiated on worker")
+                        .register(meterRegistry);
+
+        encryptEndCounter =
+                Counter.builder("encrypt_end_worker_request")
+                        .description("Number of encrypt request terminated on worker")
+                        .register(meterRegistry);
+        decryptEndCounter =
+                Counter.builder("decrypt_end_worker_request")
+                        .description("Number of decrypt request terminated on worker")
+                        .register(meterRegistry);
+    }
     @PostMapping(path = "/encrypt")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     public String encryptData(@RequestBody String data, @PathVariable(name = "keyName") String keyName) throws EncryptException {
-        return encryptionService.encrypt(keyName, data);
+        encryptInitCounter.increment();
+        String res = encryptionService.encrypt(keyName, data);
+        encryptEndCounter.increment();
+        return res;
     }
 
     @PostMapping(path = "/decrypt")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     public String decryptData(@RequestBody String data, @PathVariable(name="keyName") String keyName) throws DecryptException {
-        return encryptionService.decrypt(keyName, data);
+        decryptInitCounter.increment();
+        String res = encryptionService.decrypt(keyName, data);
+        decryptEndCounter.increment();
+        return res;
     }
 }
